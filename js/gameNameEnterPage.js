5fc2baf34b23c17274735d0f6b938ddcf90bcaeb
정현진 <s2113@e-mirim.hs.kr>
2023-05-12 01:12:16 +0900

    #8 input에 입력한 데이터 배열에 저장.

---
+++ js/gameNameEnterPage.js
@@ -0,0 +1,7 @@
+var nameArray = [];
+
+function saveName(){
+    var name = document.getElementById("NameInput").value;
+    // alert(name);
+    nameArray.push(name); // text 변수를 textArray 배열에 추가
+}
