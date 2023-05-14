f62e3ff2010dd70d5d69c79a8de0a715052e78c8
정현진 <s2113@e-mirim.hs.kr>
2023-05-14 22:48:35 +0900

    티처블머신 테스트 후 종료

--- js/gameNameEnterPage.js
+++ js/gameNameEnterPage.js
@@ -1,7 +1,7 @@
-var nameArray = [];
-
 function saveName(){
     var name = document.getElementById("NameInput").value;
-    // alert(name);
+
     nameArray.push(name); // text 변수를 textArray 배열에 추가
+    alert(name); //변수에 값 들어온 것 확인
 }
+
