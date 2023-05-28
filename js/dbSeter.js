9f01fb6acc4ba3e7e909b0d6420029fb6ad254ec
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-05-28 18:07:16 +0900

    #12 테이블 생성 완료

--- js/dbSeter.js
+++ js/dbSeter.js
@@ -1,22 +1,36 @@
 // mySQL 모듈 가져오기
-let mysql = require('mysql');
-let conn = mysql.createConnection({
+const mysql = require('mysql');
+const conn = mysql.createConnection({
         host: '127.0.0.1',
         port: '3306',
         user: 'RabbitPartTimer',
         password: 'rabbit2023@'
     })
-    
-// database scoredb 생성
+
+
 conn.connect(function(err){
     if(err) throw err;
     console.log("Connected!");
-    conn.query("CREATE DATABASE IF NOT EXISTS  `scoredb` DEFAULT CHARACTER SET utf8", function(err, result){
+
+    // database partTimerDB 생성
+    conn.query("CREATE DATABASE IF NOT EXISTS  partTimerDB DEFAULT CHARACTER SET utf8", function(err, result){
         if(err) console.log(err);
         console.log("Database created");
+        // table scoreTable 생성
+        
     });
-    conn.end();
-    console.log("Database end");
+    conn.query("USE partTimerDB", function(err,ersult){
+        if(err) console.log(err); 
+        console.log("partTimerDB useing");
+
+        // table scoreTable 생성
+        conn.query("CREATE TABLE IF NOT EXISTS scoreTable("+
+            "userName TEXT NOT NULL, "+
+            "userScore int)", function(err,ersult){
+            if(err) console.log(err); 
+            console.log("TABlLE created");
+            conn.end();
+            console.log("Database end");
+        });
+    });
 });
-
-
