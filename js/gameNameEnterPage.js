f0e6ec4c59fdc4f58606cf833a83cbec5ee6c2a4
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-05-30 01:14:15 +0900

    #14 데이터 베이스에 저장 작업중

--- js/gameNameEnterPage.js
+++ js/gameNameEnterPage.js
@@ -1,7 +1,30 @@
-function saveName(){
-    var name = document.getElementById("NameInput").value;
+// mySQL 모듈 가져오기
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'RabbitPartTimer',
+    password: 'rabbit2023@',
+    database : 'partTimerDB'
+});
 
-    nameArray.push(name); // text 변수를 textArray 배열에 추가
+conn.connect();
+
+var name = "들어가라얍";
+
+function saveName(){
+    name = document.getElementById("NameInput").value;
     alert(name); //변수에 값 들어온 것 확인
+
 }
 
+conn.connect(function(){
+    conn.query(`INSERT INTO scoreTable(userName) VALUES ("${name}")`, function(err, result){
+        if(err) console.log(err);
+        console.log("userName inserted");
+        // table scoreTable 생성
+    });
+});
+
+
+conn.end();
