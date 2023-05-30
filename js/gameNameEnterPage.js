9e2ea8b05820c36c735a3d848f1adfa548285040
yes25 <programmer.yes25@gmail.com>
2023-05-30 19:39:02 +0900

    #14 노드에서 돌리는 방식으로 바꾸는 중

--- js/gameNameEnterPage.js
+++ js/gameNameEnterPage.js
@@ -8,23 +8,26 @@ const conn = mysql.createConnection({
     database : 'partTimerDB'
 });
 
-conn.connect();
+// conn.connect();
 
 var name = "들어가라얍";
 
 function saveName(){
     name = document.getElementById("NameInput").value;
     alert(name); //변수에 값 들어온 것 확인
-
-}
-
-conn.connect(function(){
-    conn.query(`INSERT INTO scoreTable(userName) VALUES ("${name}")`, function(err, result){
+    conn.query(`INSERT INTO scoreTable(userName, userScore) VALUES (?, null)`,[name], function(err, result){
         if(err) console.log(err);
         console.log("userName inserted");
         // table scoreTable 생성
     });
-});
+    dbconn.query('select * from todotbl', (err, results)=>{
+        if(err){
+            console.log('db select error '+err);
+        }else{
+            console.dir(results);
+            res.render('list', {datalist: results});
+        }
+    })
+}
 
-
-conn.end();
+// conn.end();
