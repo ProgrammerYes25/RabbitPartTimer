3b62b6971d9561f84bf23600fb4e0610bd43a0a9
gahyun <s2112@e-mirim.hs.kr>
2023-06-13 11:51:02 +0900

    게임 시작 전 타이머 문구 및 CSS 수정

--- views/gameCountPage.js
+++ views/gameCountPage.js
@@ -9,13 +9,13 @@ var x = setInterval(function () {
    //parseInt() : 정수를 반환
    sec = time % 60; //나머지를 계산
 
-   document.getElementById("T").innerHTML = sec + "초";
+   document.getElementById("T").innerHTML = sec;
    time--;
 
    //타임아웃 시
    if (time < 0) {
       clearInterval(x); //setInterval() 실행을 끝냄
-      document.getElementById("T").innerHTML = "0초";
+      document.getElementById("T").innerHTML = "";
       // location.href = "/gamePlayPage?count=" + count; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<타이머 끝나면 화면 넘어가는 코드
       location.href = "/gamePlayPage"; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<타이머 끝나면 화면 넘어가는 코드
       // 겜 화면 수정하느라 주석달아놓음 / 나중에 풀것!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
